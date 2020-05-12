6b1446abe6a9b0287a058b34bde8b6b154870cde
Hairash <maslevian@yandex.ru>
2020-05-12 19:12:35 +0300

    Fixes for #7: * rename some variables * change some comment * some refactoring * add class Hex

--- frontend/script.js
+++ frontend/script.js
@@ -7,23 +7,22 @@ PIXI.utils.sayHello(type);
 window.onload = start;
 
 function start() {
-    //Create a Pixi Application
     let app = new PIXI.Application({ 
-        width: 256,         // default: 800
-        height: 256,        // default: 600
-        antialias: true,    // default: false
-        transparent: false, // default: false
-        resolution: 1       // default: 1
+        width: 256,
+        height: 256,
+        antialias: true,
+        transparent: false,
+        resolution: 1
     });
     app.renderer.backgroundColor = 0xd6b609;
 
-    app.renderer.autoDensity = true; // autoResize is deprecated
+    app.renderer.autoDensity = true;
     app.renderer.resize(window.innerWidth, window.innerHeight);
 
     //Add the canvas that Pixi automatically created for you to the HTML document
     document.body.appendChild(app.view); // app.view - canvas element
 
-    grid = new Hex_grid(app.stage, 8, 6, 50, 100, 100);
+    grid = new Hex_grid(app.stage, 6, 8, 50, 100, 100);
     grid.draw();
 
     grid.fill_hex(2, 5);
