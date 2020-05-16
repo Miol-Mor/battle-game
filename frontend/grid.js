d0c46a06ddad4bcb9c74c8de80c6eb0a3588568a
Hairash <maslevian@yandex.ru>
2020-05-16 10:07:33 +0300

    Add units #6: * Add Unit class * Add changes to Hex class to operate with units * Remane Grid.cells -> Grid.hexs, Grid.cell_size -> Grid.hex_size * Create units in main program

--- frontend/grid.js
+++ frontend/grid.js
@@ -7,8 +7,10 @@ class Hex extends PIXI.Graphics {
         this.points = this.hex_points(side);
         //Use drawPolygon to define the hex as a path array of x/y positions
         this.drawPolygon(this.points);
-    }
 
+        this.unit = null;
+    }
+    
     // return array of points of hex with center in (0, 0) point and given side
     hex_points(side) {
         let points = [
@@ -21,8 +23,22 @@ class Hex extends PIXI.Graphics {
         ];
         return points;
     }
-}
+    
+    // set and draw unit here
+    set_unit(unit) {
+        this.unit = unit;
+        this.draw_unit(unit);
+    }
 
+    // draw unit here
+    draw_unit(unit) {        
+        this.addChild(unit.sprite);
+    }
+
+    unset_unit() {
+        this.unit = null;
+    }
+}
 
 
 class Hex_grid {
@@ -34,17 +50,16 @@ class Hex_grid {
     // row_n, col_n - number of rows and columns in grid
     // cellsize - side of a hexcell (length of its edge)
     // x_offset, y_offset - left and top offsets inside stage
-    constructor(stage, row_n = 6, col_n = 8, cell_size = 50, x_offset = 50, y_offset = 50) {
+    constructor(stage, row_n = 6, col_n = 8, hex_size = 50, x_offset = 50, y_offset = 50) {
         this.stage = stage;
         this.row_n = row_n;
         this.col_n = col_n;
-        this.cell_size = cell_size;
+        this.hex_size = hex_size;
         this.x_offset = x_offset;
         this.y_offset = y_offset;
-        // cells - matrix of cells (call - cells[y][x], where y - number of row, x - number of column)
-        this.cells = [];
+        // hexs - matrix of hexs (call - hexs[y][x], where y - number of row, x - number of column)
+        this.hexs = [];
     }
-
 
     // draw grid on the stage
     draw() {
@@ -52,11 +67,11 @@ class Hex_grid {
         this.stage.addChild(grid_container);
         grid_container.position.set(this.x_offset, this.y_offset);
 
-        let side = this.cell_size;
+        let side = this.hex_size;
         for (let y = 0; y < this.row_n; y++) {
-            this.cells[y] = [];
+            this.hexs[y] = [];
             for (let x = 0; x < this.col_n; x++) {
-                let cur_hex = new Hex(this.cell_size, this.BORDER_WIDTH, this.BORDER_COLOR);
+                let cur_hex = new Hex(this.hex_size, this.BORDER_WIDTH, this.BORDER_COLOR);
                 
                 let y_offset = side;
                 let x_offset = side * Math.sqrt(3) / 2;
@@ -68,15 +83,14 @@ class Hex_grid {
                 
                 cur_hex.position.set(x_coord, y_coord);
                 grid_container.addChild(cur_hex);
-                this.cells[y].push(cur_hex);
+                this.hexs[y].push(cur_hex);
             }
         }
     }
 
-
     // fill hex in row y, column x with FILLCOLOR
     fill_hex(y, x) {
-        let cur_hex = this.cells[y][x];
+        let cur_hex = this.hexs[y][x];
 
         cur_hex.clear();
         cur_hex.lineStyle(1, this.BORDER_COLOR, 1);
@@ -84,6 +98,4 @@ class Hex_grid {
         cur_hex.drawPolygon(cur_hex.points);
         cur_hex.endFill();
     }
-
-    
 }
--- frontend/script.js
+++ frontend/script.js
@@ -20,14 +20,40 @@ function start() {
     // console.log(app);
     // console.log(app.renderer.view.width, app.renderer.view.height);
 
-    app.renderer.autoDensity = true; // autoResize is deprecated
+    // resize PIXI canvas acording to size of window
+    app.renderer.autoDensity = true;
     app.renderer.resize(window.innerWidth, window.innerHeight);
 
-    //Add the canvas that Pixi automatically created for you to the HTML document
+    // add the canvas that Pixi automatically created for you to the HTML document
     document.body.appendChild(app.view); // app.view - canvas element
 
+    // load images
+    app.loader.add('red unit', 'images/red unit.png');
+    app.loader.add('blue unit', 'images/blue unit.png');
+    app.loader.load(() => {loaded(app)});
+}
+
+function loaded(app) {
     grid = new Hex_grid(app.stage, 6, 8, 50, 100, 100);
     grid.draw();
 
     grid.fill_hex(2, 5);
+
+    blue_unit = new Unit(app.loader.resources["blue unit"].texture, grid, 1, 3, {HP: 10, damage: 3});
+    red_unit = new Unit(app.loader.resources["red unit"].texture, grid, 2, 0, {HP: 5, damage: 4});
+    red_unit2 = new Unit(app.loader.resources["red unit"].texture, grid, 4, 5, {HP: 7, damage: 2});
+    // blue_unit2 = new Unit(app.loader.resources["blue unit"].texture, grid, 4, 5, {HP: 10, damage: 3});
+
+    blue_unit.move_to(1, 0);
+    red_unit2.move_to(0, 3);
+    // grid.hexs[0][0].addChild(blue_unit.sprite)
+
+    for (let y = 0; y < grid.row_n; y++) {
+        for (let x = 0; x < grid.col_n; x++) {
+            if (grid.hexs[y][x].unit) {
+                console.log(y, x);
+                console.log(grid.hexs[y][x].unit.params);
+            }
+        }
+    }
 }
