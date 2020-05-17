dfe903361a4d249624f269d3bd632db07daee08c
Voronwe Sul <voronwe.s@protonmail.com>
2020-05-17 22:23:25 +0300

    Add echo socket
    Socket waite for connections on port 9001
    It spawns new thread for every connection and
    print and return everything that comes to socket

---
+++ backend/src/socket.rs
@@ -0,0 +1,60 @@
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
+use std::thread::spawn;
+use tungstenite::server::accept;
+
+pub struct Socket {
+    server: TcpListener,
+}
+
+impl Socket {
+    pub fn new(port: u16) -> Result<Socket, std::io::Error> {
+        let address = SocketAddrV4::new(Ipv4Addr::LOCALHOST, port);
+
+        match TcpListener::bind(address) {
+            Ok(server) => Ok(Socket { server }),
+            Err(error) => Err(error),
+        }
+    }
+
+    // TODO:
+    // * Catch error globaly, not in every socket usage
+    // * Match error types and exit thread for ConnectionAbort and similar errors
+    //      and do something else on other error types (like timeout)
+    pub fn listen(self) {
+        for stream in self.server.incoming() {
+            spawn(move || {
+                let stream = match stream {
+                    Ok(stream) => stream,
+                    Err(error) => panic!("Stream error: {}", error),
+                };
+
+                let mut socket = match accept(stream) {
+                    Ok(socket) => socket,
+                    Err(error) => panic!("Stream accept error: {}", error),
+                };
+
+                loop {
+                    let msg = match socket.read_message() {
+                        Ok(msg) => msg,
+                        Err(error) => {
+                            println!("Error reading message: {}", error);
+                            break;
+                        }
+                    };
+
+                    // We do not want to send back ping/pong messages.
+                    if msg.is_binary() || msg.is_text() {
+                        println!("{}", msg);
+                        match socket.write_message(msg) {
+                            Ok(_) => (),
+                            Err(error) => {
+                                print!("Error writing message: {}", error);
+                                break;
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
