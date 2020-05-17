f0491cb689a5ed7ce5f4ce542780b24a41aede64
Voronwe Sul <voronwe.s@protonmail.com>
2020-05-17 22:23:25 +0300

    Add echo socket
    Socket waite for connections on port 9001
    It spawns new thread for every connection and
    print and return everything that comes to socket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,11 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use std::thread::spawn;
+
 #[macro_use]
 extern crate rocket;
+
+mod socket;
 
 #[get("/connect")]
 fn connect() -> &'static str {
@@ -13,7 +17,18 @@ fn rocket() -> rocket::Rocket {
 }
 
 fn main() {
+    let port = 9001;
+    let socket = socket::Socket::new(port).unwrap();
+
+    // Socket echo server
+    let handle = spawn(move || {
+        socket.listen();
+    });
+
+    // Rest part
     rocket().launch();
+
+    handle.join().unwrap();
 }
 
 #[cfg(test)]
