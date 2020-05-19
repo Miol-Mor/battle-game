a788d636576d06967d1411d53bcbb43d00c77920
Hairash <maslevian@yandex.ru>
2020-05-19 14:44:31 +0300

    Change architecture #15
    * add class Game
    * units have no methods
    * game creates and moves units

--- frontend/script.js
+++ frontend/script.js
@@ -6,51 +6,19 @@ PIXI.utils.sayHello(type);
 
 window.onload = start;
 
-function start() {
-    let app = new PIXI.Application({
-        width: 256,
-        height: 256,
-        antialias: true,
-        transparent: false,
-        resolution: 1
-    });
-    app.renderer.backgroundColor = 0xd6b609;
+async function start() {
+    game = new Game();
+    await game.start();
 
-    // resize PIXI canvas acording to size of window
-    app.renderer.autoDensity = true;
-    app.renderer.resize(window.innerWidth, window.innerHeight);
-
-    // add the canvas that Pixi automatically created for you to the HTML document
-    document.body.appendChild(app.view); // app.view - canvas element
-
-    // load images
-    app.loader.add('red unit', 'images/red unit.png');
-    app.loader.add('blue unit', 'images/blue unit.png');
-    app.loader.load(() => {loaded(app)});
-}
-
-function loaded(app) {
-    grid = new Hex_grid(app.stage, 6, 8, 50, 100, 100);
-    grid.draw();
-
-    grid.fill_hex(2, 5);
-
-    blue_unit = new Unit(app.loader.resources["blue unit"].texture, grid, 1, 3, {HP: 10, damage: 3});
-    red_unit = new Unit(app.loader.resources["red unit"].texture, grid, 2, 0, {HP: 5, damage: 4});
-    red_unit2 = new Unit(app.loader.resources["red unit"].texture, grid, 4, 5, {HP: 7, damage: 2});
-    // blue_unit2 = new Unit(app.loader.resources["blue unit"].texture, grid, 4, 5, {HP: 10, damage: 3});
-
-    blue_unit.move_to(4, 0);
-    red_unit2.move_to(3, 5);
-    // grid.hexs[0][0].addChild(blue_unit.sprite)
-
-    for (let y = 0; y < grid.row_n; y++) {
-        for (let x = 0; x < grid.col_n; x++) {
-            if (grid.hexs[y][x].unit) {
+    // inspect all our units
+    for (let y = 0; y < game.grid.row_n; y++) {
+        for (let x = 0; x < game.grid.col_n; x++) {
+            if (game.grid.hexs[y][x].unit) {
                 console.log(y, x);
-                console.log(grid.hexs[y][x].unit.params);
+                console.log(game.grid.hexs[y][x].unit.params);
             }
         }
     }
-}
 
+    game.move_unit(1, 3, 5, 0);
+}
--- frontend/unit.js
+++ frontend/unit.js
@@ -1,16 +1,10 @@
 class Unit {
-    constructor(texture, grid, y, x, params) {
+    constructor(texture, img_size, params) {
         this.sprite = new PIXI.Sprite(texture);
-        this.grid = grid;
-        this.y = y;
-        this.x = x;
         this.params = params;
 
-        this.hex = this.grid.hexs[this.y][this.x];
-        
         this.sprite.anchor.set(0.5, 0.5);
-        this.scale_sprite(this.sprite, this.grid.hex_size);
-        this.set_n_draw();
+        this.scale_sprite(this.sprite, img_size);
     }
 
     // scale sprite to fit size
@@ -18,22 +12,5 @@ class Unit {
         let scale = size / Math.max(sprite.width, sprite.height);
         sprite.width *= scale;
         sprite.height *= scale;
-    }
-
-    // set unit in the hex and draw it
-    set_n_draw() {
-        this.hex.set_unit(this);
-    }
-
-    move_to(y, x) {
-        this.hex.unset_unit();
-        this.set_new_coords(y, x);
-        this.hex.set_unit(this);
-    }
-
-    set_new_coords(y, x) {
-        this.y = y;
-        this.x = x;
-        this.hex = this.grid.hexs[y][x];
     }
 }
