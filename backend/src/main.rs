4a3f4530f95d4982b47bcea11c9cc188ea3bf7af
Voronwe Sul <voronwe.s@protonmail.com>
2020-05-23 17:31:47 +0300

    Add logger as middleware

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,11 @@
+use actix_web::middleware::Logger;
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
 
 mod websocket;
+
+#[macro_use]
+extern crate log;
 
 async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     ws::start(websocket::Websocket {}, &req, stream)
@@ -9,8 +13,14 @@ async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, E
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
-        .bind("127.0.0.1:8088")?
-        .run()
-        .await
+    pretty_env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/ws/", web::get().to(index))
+            .wrap(Logger::default())
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
