955cffec51b952bd85e4085407f57c6c95e14e73
alex-eg <alexey.e.egorov@gmail.com>
2020-06-06 12:56:18 +0300

    Add healthcheck test

--- backend/src/handlers/health.rs
+++ backend/src/handlers/health.rs
@@ -13,3 +13,16 @@ pub async fn get_health() -> Result<Json<HealthResponse>, Error> {
         version: env!("CARGO_PKG_VERSION").into(),
     }))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[actix_rt::test]
+    async fn test_get_health() {
+        let response = get_health().await.unwrap();
+        let inner = response.into_inner();
+        assert_eq!(inner.status, "ok".to_string());
+        assert_eq!(inner.version, env!("CARGO_PKG_VERSION").to_string());
+    }
+}
