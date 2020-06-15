a0e63ebbc911c1e2f1703dcc8be52375b87bf41d
alex-eg <alexey.e.egorov@gmail.com>
2020-06-15 10:26:26 +0300

    Add auth mod with argon2 hashing support

---
+++ backend/src/auth.rs
@@ -0,0 +1,10 @@
+use crate::config::CONFIG;
+use crate::errors::ApiError;
+use argon2rs::argon2i_simple;
+
+pub fn hash(password: &str) -> String {
+    argon2i_simple(&password, &CONFIG.auth_salt)
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect()
+}
