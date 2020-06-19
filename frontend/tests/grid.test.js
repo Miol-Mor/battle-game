aa2cfd6ad1cb7a1ac816ccb237f8842bff5e5ff1
Hairash <maslevian@yandex.ru>
2020-06-19 12:27:51 +0300

    Add Webpack to front #21 #8
    * Tests and project work correct
    * Add local webserver
    * Put files in folders
    * Add PixiJS dependency
    
    Problems:
    * Code in browser is unreadable

---
+++ frontend/tests/grid.test.js
@@ -0,0 +1,9 @@
+import * as PIXI from 'pixi.js';
+import { Hex_grid, Hex } from '../src/grid.js';
+
+test('Default grid', () => {
+    let app = new PIXI.Application();
+    document.body.appendChild(app.view);
+    let grid = new Hex_grid(app);
+    expect(grid.row_n).toBe(6);
+});
