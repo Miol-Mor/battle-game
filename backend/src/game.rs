ae7be7ab2cac6c1deedf010b63c261f2df2dd6fd
alex-eg <alexey.e.egorov@gmail.com>
2020-06-28 12:58:25 +0300

    Swap x and y in backend grid enumerators
    X is horizontal axis, Y is vertical, but we enumerate hexes row-first,
    so Y should be the first argument in accessor functions.

--- backend/src/game.rs
+++ backend/src/game.rs
@@ -20,8 +20,8 @@ impl Game {
         }
     }
 
-    pub fn set_unit(&mut self, x: u32, y: u32, unit: Unit) -> Result<(), &str> {
-        match self.field.get_hex(x, y) {
+    pub fn set_unit(&mut self, y: u32, x: u32, unit: Unit) -> Result<(), &str> {
+        match self.field.get_hex(y, x) {
             Some(hex) => {
                 hex.unit = Some(unit);
                 Ok(())
@@ -30,8 +30,8 @@ impl Game {
         }
     }
 
-    pub fn set_content(&mut self, x: u32, y: u32, content: Content) -> Result<(), &str> {
-        match self.field.get_hex(x, y) {
+    pub fn set_content(&mut self, y: u32, x: u32, content: Content) -> Result<(), &str> {
+        match self.field.get_hex(y, x) {
             Some(hex) => {
                 hex.content = Some(content);
                 Ok(())
@@ -80,18 +80,18 @@ mod test {
             speed,
         };
 
-        let res = game.set_unit(x1, y1, unit.clone());
+        let res = game.set_unit(y1, x1, unit.clone());
         assert!(res.is_ok());
-        assert!(game.field.get_hex(x1, y1).unwrap().unit.is_some());
-        assert!(game.field.get_hex(x1, y1).unwrap().content.is_none());
+        assert!(game.field.get_hex(y1, x1).unwrap().unit.is_some());
+        assert!(game.field.get_hex(y1, x1).unwrap().content.is_none());
 
-        let field_unit = game.field.get_hex(x1, y1).unwrap().unit.as_ref().unwrap();
+        let field_unit = game.field.get_hex(y1, x1).unwrap().unit.as_ref().unwrap();
         assert_eq!(field_unit.player, player);
         assert_eq!(field_unit.hp, hp);
         assert_eq!(field_unit.attack, attack);
         assert_eq!(field_unit.speed, speed);
 
-        let res = game.set_unit(x2, y2, unit);
+        let res = game.set_unit(y2, x2, unit);
         assert!(res.is_err());
         assert_eq!(res.unwrap_err(), "no hex");
     }
@@ -108,12 +108,12 @@ mod test {
         let mut game = Game::new(5, 5);
         let wall = Wall {};
 
-        let res = game.set_content(x1, y1, Content::Wall(wall.clone()));
+        let res = game.set_content(y1, x1, Content::Wall(wall.clone()));
         assert!(res.is_ok());
-        assert!(game.field.get_hex(x1, y1).unwrap().unit.is_none());
-        assert!(game.field.get_hex(x1, y1).unwrap().content.is_some());
+        assert!(game.field.get_hex(y1, x1).unwrap().unit.is_none());
+        assert!(game.field.get_hex(y1, x1).unwrap().content.is_some());
 
-        let res = game.set_content(x2, y2, Content::Wall(wall));
+        let res = game.set_content(y2, x2, Content::Wall(wall));
         assert!(res.is_err());
         assert_eq!(res.unwrap_err(), "no hex");
     }
