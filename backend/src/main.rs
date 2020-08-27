9748fb142a4280998f6eec39fd26249f80774403
alex-eg <alexey.e.egorov@gmail.com>
2020-08-27 22:23:24 +0300

    Add mutable AppState to backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,16 @@
 use crate::config::CONFIG;
 
+use actix::Addr;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
 
 use game::Game;
 
+use std::sync::Mutex;
+
 mod api;
+mod appstate;
 mod auth;
 mod config;
 mod database;
@@ -32,8 +36,12 @@ async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, E
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     pretty_env_logger::init();
-    HttpServer::new(|| {
+    let data = web::Data::new(appstate::AppState {
+        clients : Mutex::new(vec![]),
+    });
+    HttpServer::new(move || {
         App::new()
+            .app_data(data.clone())
             .route("/ws/", web::get().to(index))
             .wrap(Logger::default())
             .configure(database::add_user_storage)
