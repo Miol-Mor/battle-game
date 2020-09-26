9aa2834029b2a229d7d61fe84d87c8109428943e
Hairash <maslevian@yandex.ru>
2020-09-26 16:29:31 +0300

    Check if current player sends message #96
    * Communicator huge refactoring

--- backend/src/communicator.rs
+++ backend/src/communicator.rs
@@ -8,29 +8,8 @@ use crate::websocket::Websocket;
 #[rtype(result = "()")]
 pub struct Msg(pub String);
 
-#[derive(Debug)]
-pub struct Communicator {
-    addresses: Vec<Addr<Websocket>>,
-}
-
-impl Communicator {
-    pub fn new(addresses: Vec<Addr<Websocket>>) -> Communicator {
-        Communicator { addresses }
-    }
-
-    pub fn broadcast<T: Serialize>(&self, msg: &T) {
-        self.send_message(msg, vec![]);
-    }
-
-    pub fn broadcast_everyone_but<T: Serialize>(&self, msg: &T, blacklisted: Addr<Websocket>) {
-        self.send_message(msg, vec![blacklisted]);
-    }
-
-    fn send_message<T: Serialize>(&self, msg: &T, blacklist: Vec<Addr<Websocket>>) {
-        for c in &self.addresses {
-            if !blacklist.contains(&c) {
-                c.do_send(Msg(serde_json::to_string(&msg).unwrap()));
-            }
-        }
+pub fn broadcast<T: Serialize>(msg: &T, recipients: Vec<Addr<Websocket>>) {
+    for c in recipients {
+        c.do_send(Msg(serde_json::to_string(&msg).unwrap()));
     }
 }
