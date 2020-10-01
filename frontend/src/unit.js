36fccf14ed7bd4fc491b66b7b18cb071f50540c1
Voronwe Sul <voronwe.s@protonmail.com>
2020-10-01 21:34:57 +0300

    Selected unit now pulsing

--- frontend/src/unit.js
+++ frontend/src/unit.js
@@ -7,6 +7,10 @@ export class Unit {
 
         this.sprite.anchor.set(0.5, 0.5);
         this.scale_sprite(img_size);
+
+        this.origin_img_size = img_size;
+        this.pulse_state = false;
+        this.pulse_count = 1;
     }
 
     // scale sprite to fit size
@@ -14,5 +18,24 @@ export class Unit {
         let scale = size / Math.max(this.sprite.width, this.sprite.height);
         this.sprite.width *= scale;
         this.sprite.height *= scale;
+    }
+
+    start_pulse() {
+        this.pulse_state = true;
+    }
+
+    stop_pulse() {
+        this.pulse_state = false;
+        this.pulse_count = 1;
+        this.scale_sprite(this.origin_img_size);
+    }
+
+    pulse() {
+        if (this.pulse_state) {
+            this.pulse_count += 0.05;
+
+            this.sprite.scale.x = Math.sin(this.pulse_count);
+            this.sprite.scale.y = Math.sin(this.pulse_count);
+        }
     }
 }
