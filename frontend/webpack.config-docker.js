d7e1d61918d9a8804c1371df74d5988fc23305fe
Voronwe Sul <voronwe.s@protonmail.com>
2021-04-17 13:24:25 +0300

    Docker files

---
+++ frontend/webpack.config-docker.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '.'),
+    hot: true,
+    host: "0.0.0.0"
+  }
+};
